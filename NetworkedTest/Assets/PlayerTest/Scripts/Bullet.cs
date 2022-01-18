fb1439b8ec35941f8a8555983c2b1bdd460180c0
Sam Badman <samuelbadman@outlook.com>
2022-01-18 21:36:09 +0000

    Added player health debug text and player bullet damage

--- NetworkedTest/Assets/PlayerTest/Scripts/Bullet.cs
+++ NetworkedTest/Assets/PlayerTest/Scripts/Bullet.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Netcode;
 
 public class Bullet : MonoBehaviour
 {
     [SerializeField] private float fireForceStrength = 50.0f;
+    [SerializeField] private int bulletDamage = 10;
 
     private Rigidbody rb;
 
@@ -16,24 +18,51 @@ public class Bullet : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        rb.AddForce(Vector3.forward * fireForceStrength);
+        FireBullet();
     }
 
     // Update is called once per frame
     private void Update()
     {
+        CheckDestroyBullet_ServerRPC();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        OnTrigger_ServerRPC(other);
+    }
+
+    [ServerRpc]
+    private void FireBullet()
+    {
+        rb.AddForce(Vector3.forward * fireForceStrength);
+    }
+
+    [ServerRpc]
+    private void CheckDestroyBullet_ServerRPC()
+    {
         if (transform.position.z >= 1000.0f)
             Destroy_ServerRPC();
     }
 
+    [ServerRpc]
     private void Destroy_ServerRPC()
     {
         // Netcode automatically destroys objects on clients that are destroyed on the server
         Destroy(gameObject);
     }
 
-    private void OnTriggerEnter(Collider other)
+    [ServerRpc]
+    private void OnTrigger_ServerRPC(Collider other)
     {
+        // Test if the hit object was a player
+        var playerTestController = other.gameObject.GetComponent<PlayerTestPlayerController>();
+
+        if (playerTestController)
+        {
+            playerTestController.Damage_ServerRPC(bulletDamage);
+        }
+
         Destroy_ServerRPC();
     }
 }
--- NetworkedTest/Assets/PlayerTest/Scripts/PlayerTestPlayerController.cs
+++ NetworkedTest/Assets/PlayerTest/Scripts/PlayerTestPlayerController.cs
@@ -9,13 +9,20 @@ public class PlayerTestPlayerController : NetworkBehaviour
     [SerializeField] private float speed = 1.0f;
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpawnDistance = 2.0f;
+    [SerializeField] private int defaultHitPointsValue = 100;
 
     private Rigidbody rb;
     private PlayerControls playerControls;
     private Camera followCam;
     private TextMesh healthText;
 
-    private int healthPoints = 100;
+    private NetworkVariable<int> healthPoints = new NetworkVariable<int>();
+
+    [ServerRpc(RequireOwnership = false)]
+    public void Damage_ServerRPC(int amount)
+    {
+        healthPoints.Value -= amount;
+    }
 
     // Awake is called before start
     private void Awake()
@@ -25,6 +32,7 @@ public class PlayerTestPlayerController : NetworkBehaviour
         rb = GetComponent<Rigidbody>();
         followCam = GetComponentInChildren<Camera>();
         healthText = GetComponentInChildren<TextMesh>();
+        healthText.text = "Health: " + defaultHitPointsValue.ToString();
     }
 
     // Start is called before the first frame update
@@ -36,8 +44,19 @@ public class PlayerTestPlayerController : NetworkBehaviour
             followCam.gameObject.SetActive(false);
         }
 
-        // Set health value
-        healthText.text = "Health: " + healthPoints.ToString();
+        healthPoints.Value = defaultHitPointsValue;
+    }
+
+    private void OnEnable()
+    {
+        // Register callback for when the health points value is changed
+        healthPoints.OnValueChanged += OnHealthPointsChanged;
+    }
+
+    private void OnDisable()
+    {
+        // Deregister callback for when the health points value is changed
+        healthPoints.OnValueChanged -= OnHealthPointsChanged;
     }
 
     // Update is called once per frame
@@ -72,7 +91,7 @@ public class PlayerTestPlayerController : NetworkBehaviour
 
     private void Shoot_performed(InputAction.CallbackContext obj)
     {
-        // Spawn a prefab on the server and add it to all client worlds
+        // Spawn a prefab on the server and spawn on all client
         if(IsLocalPlayer)
         {
             SpawnBullet_ServerRPC(transform.position);
@@ -86,6 +105,7 @@ public class PlayerTestPlayerController : NetworkBehaviour
             playerPosition + (Vector3.forward * bulletSpawnDistance),
             Quaternion.identity);
 
+        // Need to call spawn on the network object component to spawn on clients
         gameObject.GetComponent<NetworkObject>().Spawn(true);
     }
 
@@ -99,5 +119,13 @@ public class PlayerTestPlayerController : NetworkBehaviour
         if (NetworkManager.Singleton.IsHost) GUILayout.Label("Host (Server/Client)");
         else if (NetworkManager.Singleton.IsClient) GUILayout.Label("Client");
         else if (NetworkManager.Singleton.IsServer) GUILayout.Label("Server");
+    }
+
+    private void OnHealthPointsChanged(int oldHealthPoints, int newHealthPoints)
+    {
+        if (!IsClient) return;
+
+        // Update the text to display the new health points value
+        healthText.text = "Health: " + healthPoints.Value.ToString();
     }
 }
